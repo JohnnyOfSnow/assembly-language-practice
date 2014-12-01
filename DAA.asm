c1aa426b35034dcfd312edd0330d68dc2843533a
櫻雪紛飛 <good258johnny@yahoo.com.tw>
2014-12-01 14:21:35 +0800

    加入邏輯註解   但輔助進位還沒解決

--- DAA.asm
+++ DAA.asm
@@ -12,7 +12,7 @@ main PROC
 	call WriteString
 
 	mov al,35h
-	add al,48h
+	add al,65h
 	call DAAItself
 	call WriteHex
 	exit
@@ -21,9 +21,9 @@ main ENDP
 DAAItself PROC
 	push eax
 	and al,00F0h
-	.IF	al > 9h 
-		pop eax
-		add eax,6h
+	.IF	al > 9h             ;If (AL(lo) > 9) or (AuxCarry = 1)
+		pop eax				
+		add eax,6h			;AL = AL + 6
 	.ELSE
 		pop eax
 	.ENDIF
@@ -31,12 +31,12 @@ DAAItself PROC
 	push eax
 	and al,00F0h
 
-	.IF	al > 9h && CARRY?
+	.IF	al > 9h || CARRY?	;If (AL(hi) > 9) or Carry = 1
 		pop eax
-		add eax,60h
-		stc    ; carry = 1
+		add eax,60h			;AL = AL + 60h
+		stc					; carry = 1
 	.ELSE
-		clc    ; carry = 0
+		clc					; carry = 0
 		pop eax
 	.ENDIF
 	ret
