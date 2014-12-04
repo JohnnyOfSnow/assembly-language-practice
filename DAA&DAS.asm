23653b4b6a5e170ebb74a1c2a93ca159aed39e37
櫻雪紛飛 <good258johnny@yahoo.com.tw>
2014-12-04 11:36:49 +0800

    將DAA DAS合再一起

---
+++ DAA&DAS.asm
@@ -0,0 +1,159 @@
+TITLE MASM Template						(main.asm)
+
+INCLUDE Irvine32.inc
+.data
+myMessage BYTE "Assembly language program example",0dh,0ah,0
+display0 BYTE "2個16位進數相加  用BCD的方式，每次的結果請看最後2位",0
+display1 BYTE "69h + 29h",0
+display2 BYTE "35h + 48h",0
+display3 BYTE "35h + 65h",0
+display4 BYTE "2個16位進數相減  用BCD的方式，每次的結果請看最後2位",0
+display5 BYTE "85h - 48h",0
+display6 BYTE "62h - 35h",0
+display7 BYTE "32h - 29h",0
+
+.code
+main PROC
+	call Clrscr
+
+	mov	 edx,OFFSET display0
+	call WriteString
+	call Crlf
+
+	mov	 edx,OFFSET display1
+	call WriteString
+	call Crlf
+
+	mov al,69h
+	add al,29h
+	call DAAItself
+	call WriteHex
+	call Crlf
+	clc
+
+	mov	 edx,OFFSET display2
+	call WriteString
+	call Crlf
+	mov al,35h
+	add al,48h
+	call DAAItself
+	call WriteHex
+	clc
+	call Crlf
+
+	mov	 edx,OFFSET display3
+	call WriteString
+	call Crlf
+	mov al,35h
+	add al,65h
+	call DAAItself
+	call WriteHex
+	call Crlf
+
+	mov	 edx,OFFSET display4
+	call WriteString
+	call Crlf
+
+	mov	 edx,OFFSET display5
+	call WriteString
+	call Crlf
+
+	mov al,85h
+	sub al,48h
+	call DASItself
+	call WriteHex
+	call Crlf
+
+	mov	 edx,OFFSET display6
+	call WriteString
+	call Crlf
+
+	mov al,62h
+	sub al,35h
+	call DASItself
+	call WriteHex
+	call Crlf
+
+	mov	 edx,OFFSET display7
+	call WriteString
+	call Crlf
+
+	mov al,32h
+	sub al,29h
+	call DASItself
+	call WriteHex
+	call Crlf
+	exit
+main ENDP
+
+DAAItself PROC
+	
+	push eax
+	and al,000Fh
+	LAHF                    ;旗標暫存器的 bit0~bit7 的內容存入 AH
+	bt eax,20
+	jc L1
+	.IF	al > 9h             ;If (AL(lo) > 9) or (AuxCarry = 1)
+		L1:
+		pop eax				
+		add eax,6h			;AL = AL + 6
+		bts eax,20
+		SAHF
+		clc
+	.ELSE
+		pop eax
+	.ENDIF
+	
+	push eax
+	
+	and al,00F0h
+	jc L2
+	.IF	al > 90h 	;If (AL(hi) > 9) or Carry = 1
+		L2:
+		pop eax
+		add eax,60h			;AL = AL + 60h
+		push eax
+		stc					; carry = 1
+	.ELSE
+		clc					; carry = 0
+	.ENDIF
+	pop eax
+	ret
+DAAItself ENDP
+
+DASItself PROC
+	push eax
+	and al,000Fh
+	LAHF                    ;旗標暫存器的 bit0~bit7 的內容存入 AH
+	bt eax,20
+	jc L1
+	.IF	al > 9h             ;If (AL(lo) > 9) or (AuxCarry = 1)
+		L1:
+		pop eax				
+		sub eax,6h			;AL = AL - 6
+		bts eax,20
+		SAHF
+		clc
+	.ELSE
+		btr eax,20
+		pop eax
+	.ENDIF
+	
+	push eax
+	
+	and al,00F0h
+	jc L2
+	.IF	al > 9Fh 	;If (AL(hi) > 9) or Carry = 1
+		L2:
+		pop eax
+		sub eax,60h			;AL = AL - 60h
+		push eax
+		stc					; carry = 1
+	.ELSE
+		clc					; carry = 0
+	.ENDIF
+	pop eax
+	ret
+DASItself ENDP
+
+END main
